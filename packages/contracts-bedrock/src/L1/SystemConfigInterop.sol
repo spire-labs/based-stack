85b67dbcaa5c926679b1ea0748ad8cc585644a42
excaliborr <finksglasses@gmail.com>
2024-10-08 17:44:38 -0400

    fix: semver natspec

--- packages/contracts-bedrock/src/L1/SystemConfigInterop.sol
+++ packages/contracts-bedrock/src/L1/SystemConfigInterop.sol
@@ -72,7 +72,7 @@ contract SystemConfigInterop is SystemConfig {
         Storage.setAddress(DEPENDENCY_MANAGER_SLOT, _dependencyManager);
     }
 
-    /// @custom:semver +interop
+    /// @custom:semver +interop-1
     function version() public pure override returns (string memory) {
         return string.concat(super.version(), "+interop-1");
     }
