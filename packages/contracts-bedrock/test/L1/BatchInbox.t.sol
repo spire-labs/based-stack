a387a9740d08d324fdffe14ec2b4e1ed98cfa7f0
wintercode <contact@wintercode.co>
2024-10-10 00:34:09 +0900

    uses vm.mockCall

--- packages/contracts-bedrock/test/L1/BatchInbox.t.sol
+++ packages/contracts-bedrock/test/L1/BatchInbox.t.sol
@@ -1,53 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import { Test } from "forge-std/Test.sol";
 import { BatchInbox } from "src/L1/BatchInbox.sol";
 import { Election } from "src/L1/Election.sol";
-
+import { Test } from "forge-std/Test.sol";
 import "src/libraries/BatchInboxErrors.sol";
-
-contract MockElection is Election {
-    address public winner;
-
-    function setWinner(address _winner) external {
-        winner = _winner;
-    }
-
-    function electionWinner() external view override returns (address) {
-        return winner;
-    }
-}
 
 contract BatchInboxTest is Test {
     BatchInbox public batchInbox;
-    MockElection public mockElection;
-    address public winner = address(0x123);
-    address public nonWinner = address(0x456);
+    Election public election = Election(makeAddr("election"));
+
+    address public mockWinner = address(0x123);
 
     event BatchSubmitted();
 
     function setUp() public {
-        mockElection = new MockElection();
-        batchInbox = new BatchInbox(Election(address(mockElection)));
-        mockElection.setWinner(winner);
+        batchInbox = new BatchInbox(election);
     }
 
     function testSubmitOnTargetBlockSuccess() public {
-        vm.prank(winner);
-        uint256 targetBlock = 1337;
-        vm.roll(targetBlock);
-        batchInbox.submit(targetBlock);
+        bytes memory data = abi.encodeWithSelector(Election.electionWinner.selector);
+        vm.mockCall(address(election), data, abi.encode(mockWinner));
+
+        vm.prank(mockWinner);
+        uint256 targetBlock = block.number;
+
         vm.expectEmit(true, true, true, true);
         emit BatchSubmitted();
+
+        batchInbox.submit(targetBlock);
     }
 
-    function testSubmitOnTargetBlockRevert_InvalidBlock() public {
-        vm.prank(winner);
-        uint256 targetBlock = 1337;
-        vm.roll(targetBlock + 1);
+    function testSubmitOnTargetBlockRevert_NotWinner() public {
+        bytes memory data = abi.encodeWithSelector(Election.electionWinner.selector);
+        vm.mockCall(address(election), data, abi.encode(mockWinner));
+
+        address nonWinner = address(0x456);
+        vm.prank(nonWinner);
+
+        uint256 targetBlock = block.number;
+
+        vm.expectRevert(abi.encodeWithSelector(OnlyElectionWinner.selector, nonWinner, mockWinner));
+
         batchInbox.submit(targetBlock);
-        vm.expectRevert("InvalidTargetBlock");
-        revert InvalidTargetBlock(targetBlock, targetBlock + 1);
+    }
+
+    function testSubmitOnTargetBlockRevert_InvalidBlockNumber() public {
+        bytes memory data = abi.encodeWithSelector(Election.electionWinner.selector);
+        vm.mockCall(address(election), data, abi.encode(mockWinner));
+
+        vm.prank(mockWinner);
+
+        uint256 currentBlock = block.number;
+        uint256 invalidTargetBlock = currentBlock + 1;
+
+        vm.expectRevert(abi.encodeWithSelector(InvalidTargetBlock.selector, invalidTargetBlock, currentBlock));
+
+        batchInbox.submit(invalidTargetBlock);
     }
 }
