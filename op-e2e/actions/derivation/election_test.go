6164466d168278ec7570bdd950fb44f072844921
excaliborr <finksglasses@gmail.com>
2024-12-10 08:33:42 -0500

    fix: ci

--- op-e2e/actions/derivation/election_test.go
+++ op-e2e/actions/derivation/election_test.go
@@ -62,6 +62,11 @@ func TestElectionType(gt *testing.T) {
 	miner.ActL1StartBlock(12)(t)
 	miner.ActL1IncludeTxByHash(batchTx.Hash())(t)
 	miner.ActL1EndBlock(t)
+
+	// verifier picks up the L2 chain that was submitted
+	verifier.ActL1HeadSignal(t)
+	verifier.ActL2PipelineFull(t)
+
 	require.Equal(t, verifier.L2Safe(), sequencer.L2Unsafe(), "verifier syncs from sequencer via L1")
 	require.NotEqual(t, sequencer.L2Safe(), sequencer.L2Unsafe(), "sequencer has not processed L1 yet")
 
