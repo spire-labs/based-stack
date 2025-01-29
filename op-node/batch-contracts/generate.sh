1fb551a7ef84b08c8e997898c10db633ddcefa70
excaliborr <124819095+excaliborr@users.noreply.github.com>
2025-01-29 08:15:39 -0500

    feat(batch-contracts): BatchCheckSeqConfig (#118)

--- op-node/batch-contracts/generate.sh
+++ op-node/batch-contracts/generate.sh
@@ -24,3 +24,12 @@ cat BatchRandomTicketInstruction.json | jq -r  '.abi' > BatchRandomTicketInstruc
 cd ../..
 
 abigen --abi ./out/BatchRandomTicketInstruction.sol/BatchRandomTicketInstruction.abi --bin ./out/BatchRandomTicketInstruction.sol/BatchRandomTicketInstruction.bin --pkg BatchRandomTicketInstruction --out ./bindings/BatchRandomTicketInstruction/batch_random_ticket_instruction.go
+
+# BatchCheckSeqConfig
+cd out/BatchCheckSeqConfig.sol
+
+cat BatchCheckSeqConfig.json | jq -r  '.bytecode.object' > BatchCheckSeqConfig.bin
+cat BatchCheckSeqConfig.json | jq -r  '.abi' > BatchCheckSeqConfig.abi
+cd ../..
+
+abigen --abi ./out/BatchCheckSeqConfig.sol/BatchCheckSeqConfig.abi --bin ./out/BatchCheckSeqConfig.sol/BatchCheckSeqConfig.bin --pkg BatchCheckSeqConfig --out ./bindings/BatchCheckSeqConfig/batch_check_seq_config.go
